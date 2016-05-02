c71351f6eda37f5fcdc10c8f2b618634eb7d8597
John Clema <john.clema@gmail.com>
2016-05-02 18:45:45 +0800

    feat: map regions now stored and restored

--- Virtual Tourist/MapViewController.swift
+++ Virtual Tourist/MapViewController.swift
@@ -11,16 +11,26 @@ import MapKit
 import CoreData
 import CoreLocation
 
-class MapViewController: UIViewController, MKMapViewDelegate {
+class MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var mapViewBottomLayoutGuide: NSLayoutConstraint!
     
+    var mapRegionFilePath : String {
+        let manager = NSFileManager.defaultManager()
+        let url = manager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first! as NSURL
+        return url.URLByAppendingPathComponent("mapRegionArchive").path!
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        restoreMapRegion(false)
         mapView.delegate = self
         let editButton = UIBarButtonItem(barButtonSystemItem: .Edit, target: self, action: #selector(MapViewController.showDeleteButton))
         self.navigationItem.setRightBarButtonItem(editButton, animated: true)
+        addPinDropRecognizer()
 //        showEditButton()
 //        showDeleteButton()
         // Do any additional setup after loading the view, typically from a nib.
@@ -66,6 +76,50 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         self.mapView.addAnnotation(annotation)
     }
     
+    func saveMapRegion() {
+        
+        // Place the "center" and "span" of the map into a dictionary
+        // The "span" is the width and height of the map in degrees.
+        // It represents the zoom level of the map.
+        
+        let dictionary = [
+            "latitude" : mapView.region.center.latitude,
+            "longitude" : mapView.region.center.longitude,
+            "latitudeDelta" : mapView.region.span.latitudeDelta,
+            "longitudeDelta" : mapView.region.span.longitudeDelta
+        ]
+        
+        // Archive the dictionary into the filePath
+        NSKeyedArchiver.archiveRootObject(dictionary, toFile: mapRegionFilePath)
+    }
+    
+    func restoreMapRegion(animated: Bool) {
+        
+        // if we can unarchive a dictionary, we will use it to set the map back to its
+        // previous center and span
+        if let regionDictionary = NSKeyedUnarchiver.unarchiveObjectWithFile(mapRegionFilePath) as? [String : AnyObject] {
+            
+            let longitude = regionDictionary["longitude"] as! CLLocationDegrees
+            let latitude = regionDictionary["latitude"] as! CLLocationDegrees
+            let center = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            
+            let longitudeDelta = regionDictionary["latitudeDelta"] as! CLLocationDegrees
+            let latitudeDelta = regionDictionary["longitudeDelta"] as! CLLocationDegrees
+            let span = MKCoordinateSpan(latitudeDelta: latitudeDelta, longitudeDelta: longitudeDelta)
+            
+            let savedRegion = MKCoordinateRegion(center: center, span: span)
+            
+            print("lat: \(latitude), lon: \(longitude), latD: \(latitudeDelta), lonD: \(longitudeDelta)")
+            
+            mapView.setRegion(savedRegion, animated: animated)
+        }
+    }
+    
+    
+}
+
+extension MapViewController : MKMapViewDelegate {
+    
     //Tap and hold gesture for map = drop a new pin at the tap and hold location
     
     func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
@@ -87,6 +141,12 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     
     func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
         // Segue to next view and perform request from flickr
+        print(view.annotation?.coordinate)
+    }
+    
+    func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        saveMapRegion()
     }
 }
+
 
